94968c396e1d40a2d836eaaf336269db2188e921
SIS <SIS@SIS-PC>
2018-12-28 00:28:39 +0900

    LinkedList 구현 중

---
+++ src/list/linkedlist/implementation/LinkedList.java
@@ -0,0 +1,95 @@
+package list.linkedlist.implementation;
+
+public class LinkedList {
+
+	private Node head;
+	private Node tail;
+	private int size = 0;
+
+	private class Node {
+		private Object data;
+		private Node next;
+
+		public Node(Object input) {
+			this.data = input;
+			this.next = null;
+		}
+
+		public String toString() {
+			return String.valueOf(this.data);
+		}
+	}
+
+	public void addFirst(Object input) {
+		Node newNode = new Node(input);
+		newNode.next = head;
+		head = newNode;
+		size++;
+		if (head.next == null) {
+			tail = head;
+		}
+	}
+
+	public void addLast(Object input) {
+		Node newNode = new Node(input);
+		if (size == 0) {
+			addFirst(input);
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+			size++;
+		}
+	}
+
+//	내부적인 부품이 외부에 노출되면 안됨
+//	public Node node(int index) {
+	Node node(int index) {
+		Node x = head;
+		for (int i = 0; i < index; i++) {
+			x = x.next;
+		}
+		return x;
+	}
+
+	public void add(int index, Object input) {
+		if (index == 0) {
+			addFirst(input);
+		} else {
+			Node temp1 = node(index - 1);
+			Node temp2 = temp1.next;
+			Node newNode = new Node(input);
+			temp1.next = newNode;
+			newNode.next = temp2;
+			size++;
+			if (newNode.next == null) {
+				tail = newNode;
+			}
+		}
+	}
+
+	public String toString() {
+		if (head == null) {
+			return "[]";
+		}
+
+		Node temp = head;
+		String str = "[";
+
+		while (temp.next != null) {
+			str += temp.data + ", ";
+			temp = temp.next;
+		}
+		str += temp.data;
+
+		return str + "]";
+	}
+	
+	public Object removeFirst() {
+		Node temp = head;
+		head = head.next;
+		Object returnData = temp.data;
+		temp = null;
+		size--;
+		return returnData;
+	}
+}
