ba678258217b42239a84ac7dd9953400a8214c3a
InSeong-So <goflvhxj2547@gmail.com>
2019-01-04 17:08:51 +0900

    두번째 병합 정렬 구현

--- src/main/java/implementation/Sort.java
+++ src/main/java/implementation/Sort.java
@@ -63,43 +63,95 @@ public class Sort {
 		return sorted;
 	}
 
-	// 병합 정렬 알고리즘
-	public List<Integer> mergeSort(List<Integer> values) {
-		if (values.size() < 2) {
-			return values;
-		}
+//	병합 정렬 알고리즘
+//	public List<Integer> mergeSort(List<Integer> values) {
+//		if (values.size() < 2) {
+//			return values;
+//		}
+//
+//		List<Integer> leftHalf = values.subList(0, values.size() / 2);
+//		List<Integer> rightHalf = values.subList(values.size() / 2, values.size());
+//		return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+//	}
+//
+//	병합	
+//	private List<Integer> merge(List<Integer> left, List<Integer> right) {
+//		int leftPtr = 0;
+//		int rightPtr = 0;
+//
+//		List<Integer> merged = new ArrayList<>(left.size() + right.size());
+//
+//		while (leftPtr < left.size() && rightPtr < right.size()) {
+//			if (left.get(leftPtr) < right.get(rightPtr)) {
+//				merged.add(left.get(leftPtr));
+//				leftPtr++;
+//			} else {
+//				merged.add(right.get(rightPtr));
+//				rightPtr++;
+//			}
+//		}
+//
+//		while (leftPtr < left.size()) {
+//			merged.add(left.get(leftPtr));
+//			leftPtr++;
+//		}
+//
+//		while (rightPtr < right.size()) {
+//			merged.add(right.get(rightPtr));
+//			rightPtr++;
+//		}
+//
+//		return merged;
+//	}
 
-		List<Integer> leftHalf = values.subList(0, values.size() / 2);
-		List<Integer> rightHalf = values.subList(values.size() / 2, values.size());
-		return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+	private static void mergeSort(int[] arr) {
+		int[] temp = new int[arr.length];
+		mergeSort(arr, temp, 0, arr.length - 1);
 	}
 
-	private List<Integer> merge(List<Integer> left, List<Integer> right) {
-		int leftPtr = 0;
-		int rightPtr = 0;
+	private static void mergeSort(int[] arr, int[] temp, int start, int end) {
+		if (start < end) {
+			int mid = (start + end) / 2;
+			mergeSort(arr, temp, start, mid);
+			mergeSort(arr, temp, mid + 1, end);
+			merge(arr, temp, start, mid, end);
+		}
+	}
 
-		List<Integer> merged = new ArrayList<>(left.size() + right.size());
-
-		while (leftPtr < left.size() && rightPtr < right.size()) {
-			if (left.get(leftPtr) < right.get(rightPtr)) {
-				merged.add(left.get(leftPtr));
-				leftPtr++;
+	private static void merge(int[] arr, int[] temp, int start, int mid, int end) {
+		for (int i = start; i <= end; i++) {
+			temp[i] = arr[i];
+		}
+		int part1 = start;
+		int part2 = mid + 1;
+		int index = start;
+		while (part1 <= mid && part2 <= end) {
+			if (temp[part1] <= temp[part2]) {
+				arr[index] = temp[part1];
+				part1++;
 			} else {
-				merged.add(right.get(rightPtr));
-				rightPtr++;
+				arr[index] = temp[part2];
+				part2++;
 			}
+			index++;
 		}
-
-		while (leftPtr < left.size()) {
-			merged.add(left.get(leftPtr));
-			leftPtr++;
+		for (int i = 0; i <= mid - part1; i++) {
+			arr[index + i] = temp[part1 + i];
 		}
-
-		while (rightPtr < right.size()) {
-			merged.add(right.get(rightPtr));
-			rightPtr++;
-		}
-
-		return merged;
 	}
+	
+	private static void printArray(int[] arr) {
+		for(int data : arr) {
+			System.out.print(data + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {3, 9, 4, 7, 5, 0, 1, 6, 8, 2};
+		printArray(arr);
+		mergeSort(arr);
+		printArray(arr);
+	}
+
 }
